ea73b3298e9129c6ac6e7c9d5173e1c662f3659d
Don Amfahr <donny@amfahr.com>
2011-08-29 22:49:36 -0400

    from Xcode

---
+++ SharedBills/SharedBills/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SharedBills
+//
+//  Created by Donny Amfahr on 8/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
