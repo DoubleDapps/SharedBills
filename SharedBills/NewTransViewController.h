7725ff7e7b2557c7fe78e822a79a034094707dca
Doug Friedman <douglas.p.friedman@gmail.com>
2011-08-31 22:57:35 -0400

    added table view

---
+++ SharedBills/NewTransViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewTransViewController.h
+//  SharedBills
+//
+//  Created by Doug Friedman on 8/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTransViewController : UITableViewController
+
+@end
